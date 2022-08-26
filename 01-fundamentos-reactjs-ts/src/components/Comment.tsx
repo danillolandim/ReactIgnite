9c1cdf7c868b8e4e1eae036bfbcc1766f1fa11ae
Danillo Landim <danillozinho@gmail.com>
2022-08-26 16:34:40 -0300

    first commit

---
+++ 01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import styles from './Comment.module.css';
+import { Trash } from 'phosphor-react';
+import{ThumbsUp } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+
+interface CommentsProps{
+  content: string;
+  onDeleteComment: (comment:string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentsProps){
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment(){
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment(){
+    setLikeCount((state)=> {
+      return state +1
+    });
+  }
+
+  return(
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego James</strong>
+              <time title="11 de maio às 08:13" dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash  size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+       
+       
+        <footer>
+        <button onClick={handleLikeComment}>
+          <ThumbsUp />
+          Aplaudir <span>{likeCount}</span>
+        </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
